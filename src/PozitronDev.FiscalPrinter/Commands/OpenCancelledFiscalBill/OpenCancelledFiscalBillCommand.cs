15a7cf74603cfa75fd83e1c5211e8693e5ca4927
Fati Iseni <fati.iseni@pozitrongroup.com>
2020-12-24 14:22:52 +0100

    Added OpenCancelledFiscalBill command.

---
+++ src/PozitronDev.FiscalPrinter/Commands/OpenCancelledFiscalBill/OpenCancelledFiscalBillCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PozitronDev.FiscalPrinter
+{
+    public class OpenCancelledFiscalBillCommand : IFiscalCommand<OpenCancelledFiscalBillResponse>
+    {
+        private readonly OpenCancelledFiscalBillRequest request;
+
+        public OpenCancelledFiscalBillCommand(OpenCancelledFiscalBillRequest request)
+        {
+            this.request = request;
+        }
+
+        public byte Byte => 48;
+
+        public string RequestData
+        {
+            get
+            {
+                return request.WaiterCode + "," + request.WaiterPin + "," + request.OperatorPlace + ",R";
+            }
+        }
+
+        public OpenCancelledFiscalBillResponse MapResponse(FiscalResponse fiscalResponse)
+        {
+            return new OpenCancelledFiscalBillResponse(fiscalResponse);
+        }
+    }
+}
