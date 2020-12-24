10232696ebab64326dd356be99e6258bb6e72362
Fati Iseni <fati.iseni@pozitrongroup.com>
2020-12-24 14:11:44 +0100

    Added RegisterItemOnOpenBill command.

---
+++ src/PozitronDev.FiscalPrinter/Commands/RegisterItemOnOpenBill/RegisterItemOnOpenBillCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PozitronDev.FiscalPrinter
+{
+    public class RegisterItemOnOpenBillCommand : IFiscalCommand<FiscalResponse>
+    {
+        private readonly RegisterItemOnOpenBillRequest request;
+
+        public RegisterItemOnOpenBillCommand(RegisterItemOnOpenBillRequest request)
+        {
+            this.request = request;
+        }
+
+        public byte Byte => 49;
+
+        public string RequestData
+        {
+            get
+            {
+                var temp = request.IsNationalProduct ? "@" : "";
+                var discount = request.ItemDiscountPercentage != 0 ? "," + request.ItemDiscountPercentage.ToString("0.00", System.Globalization.CultureInfo.InvariantCulture) : "";
+                return request.ItemName + "\t" + temp + request.ItemTax + request.ItemPrice.ToString("0.00", System.Globalization.CultureInfo.InvariantCulture) + "*" + request.ItemQuantity.ToString("0.000", System.Globalization.CultureInfo.InvariantCulture) + discount;
+
+            }
+        }
+
+        public FiscalResponse MapResponse(FiscalResponse fiscalResponse)
+        {
+            return fiscalResponse;
+        }
+    }
+}
---
+++ src/PozitronDev.FiscalPrinter/Commands/RegisterItemOnOpenBill/RegisterItemOnOpenBillRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PozitronDev.FiscalPrinter
+{
+    public class RegisterItemOnOpenBillRequest : FiscalRequest
+    {
+        public bool IsNationalProduct { get; set; }
+        public TaxEnum ItemTax { get; set; }
+        public string ItemName { get; set; }
+        public decimal ItemPrice { get; set; }
+        public decimal ItemQuantity { get; set; }
+        public decimal ItemDiscountPercentage { get; set; }
+    }
+}
